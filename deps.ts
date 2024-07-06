ea2a5f46329f3b3bb50acc5a47a04960582dcc08
devsheva <sheshi.mateo01@gmail.com>
2024-07-07 00:38:54 +0200

    chore: add zod package

--- deps.ts
+++ deps.ts
@@ -3,3 +3,4 @@ export * from "https://deno.land/x/grammy@v1.26.0/mod.ts";
 export * from "https://deno.land/x/grammy_conversations@v1.2.0/mod.ts";
 export * from "https://deno.land/x/grammy_storages@v2.4.2/free/src/mod.ts"
 export * from "https://deno.land/x/grammy_types@v3.10.0/mod.ts"
+export { z } from "https://deno.land/x/zod@v3.16.1/mod.ts";
