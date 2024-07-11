82ea3567aa5914881d66bbc1d635731176ac0dc0
devsheva <sheshi.mateo01@gmail.com>
2024-07-12 00:43:07 +0200

    chore(gemini-response): add zod types for request and response schema of `generateContent` api

--- src/config.ts
+++ src/config.ts
@@ -5,6 +5,10 @@ const envSchema = z.object({
   HN_API: z.string().default('https://hacker-news.firebaseio.com/v0/'),
   BOT_TOKEN: z.string().default('dummy'),
   APP_ENV: z.enum(['development', 'test', 'production']).default('development'),
+  GEMINI_API_KEY: z.string().default('dummy'),
+  BASE_GEMINI_ENDPOINT: z.string().default(
+    'https://generativelanguage.googleapis.com/v1/',
+  ),
 })
 
 try {
--- src/types.ts
+++ src/types.ts
@@ -42,3 +42,106 @@ export type PreferencesContext =
   & ConversationFlavor
 
 export type ConversationContext = Conversation<PreferencesContext>
+
+const safetyRatingSchema = z.object({
+  category: z.enum([
+    'HARM_CATEGORY_UNSPECIFIED',
+    'HARM_CATEGORY_DEROGATORY',
+    'HARM_CATEGORY_TOXICITY',
+    'HARM_CATEGORY_VIOLENCE',
+    'HARM_CATEGORY_SEXUAL',
+    'HARM_CATEGORY_MEDICAL',
+    'HARM_CATEGORY_DANGEROUS',
+    'HARM_CATEGORY_HARASSMENT',
+    'HARM_CATEGORY_HATE_SPEECH',
+    'HARM_CATEGORY_SEXUALLY_EXPLICIT',
+    'HARM_CATEGORY_DANGEROUS_CONTENT',
+  ]),
+  probability: z.enum([
+    'HIGH',
+    'MEDIUM',
+    'LOW',
+    'NEGLIGIBLE',
+    'HARM_PROBABILITY_UNSPECIFIED',
+  ]),
+  blocked: z.boolean().optional(),
+})
+
+const citationMetadataSchema = z.object({
+  citationSources: z.array(
+    z.object({
+      'startIndex': z.number().int().optional(),
+      'endIndex': z.number().int().optional(),
+      'uri': z.string().optional(),
+      'license': z.string().optional(),
+    }),
+  ),
+})
+
+const contentSchema = z.object({
+  parts: z.array(
+    z.object({
+      text: z.string().optional(),
+      inlineData: z.object({
+        mimeType: z.string(),
+        data: z.string().base64(),
+      }).optional(),
+    }),
+  ),
+  role: z.enum([
+    'user',
+    'model',
+  ]).optional(),
+})
+
+const requestContentSchema = z.object({
+  contents: z.array(contentSchema),
+})
+
+export type RequestContent = z.infer<typeof requestContentSchema>
+
+const candidateSchema = z.object({
+  content: contentSchema,
+  finishReason: z.enum(
+    [
+      'FINISH_REASON_UNSPECIFIED',
+      'STOP',
+      'MAX_TOKENS',
+      'SAFETY',
+      'RECITATION',
+      'OTHER',
+    ],
+  ).default('FINISH_REASON_UNSPECIFIED'),
+  safetyRatings: z.array(safetyRatingSchema),
+  citationMetadata: citationMetadataSchema.optional(),
+  tokenCount: z.number().int().optional(),
+  index: z.number().int().optional(),
+})
+const promptFeedbackSchema = z.object({
+  blockReason: z.enum([
+    'BLOCK_REASON_UNSPECIFIED',
+    'SAFETY',
+    'OTHER',
+  ]).optional(),
+  safetyRatings: z.array(safetyRatingSchema),
+})
+
+const usageMetadataSchema = z.object({
+  promptTokenCount: z.number(),
+  candidatesTokenCount: z.number(),
+  totalTokenCount: z.number(),
+})
+
+const generateContentResponseSchema = z.object({
+  candidates: z.array(
+    candidateSchema,
+  ),
+  promptFeedback: promptFeedbackSchema.optional(),
+  usageMetadata: usageMetadataSchema,
+})
+
+export const filteredContentSchema = generateContentResponseSchema.pick(
+  {
+    candidates: true,
+  },
+)
