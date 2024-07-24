40091ff6b02dba8c8a3f55f666a4cff35605c445
devsheva <sheshi.mateo01@gmail.com>
2024-07-24 23:01:07 +0200

    fix(config): restore test task arg when setting env

--- src/config.ts
+++ src/config.ts
@@ -1,23 +1,29 @@
-import { R, z } from "@/deps.ts";
-import { load } from "@std/dotenv";
+import { R, z } from '@/deps.ts'
+import { load } from '@std/dotenv'
+import { parseArgs } from '@std/cli'
 
 const envSchema = z.object({
-  HN_API: z.string().default("https://hacker-news.firebaseio.com/v0"),
-  BOT_TOKEN: z.string().default("dummy"),
-  APP_ENV: z.enum(["development", "test", "production"]).default("development"),
-  GEMINI_API_KEY: z.string().default("dummy"),
+  HN_API: z.string().default('https://hacker-news.firebaseio.com/v0'),
+  BOT_TOKEN: z.string().default('dummy'),
+  APP_ENV: z.enum(['development', 'test', 'production']).default('development'),
+  GEMINI_API_KEY: z.string().default('dummy'),
   BASE_GEMINI_ENDPOINT: z.string().default(
-    "https://generativelanguage.googleapis.com/v1beta",
+    'https://generativelanguage.googleapis.com/v1beta',
   ),
-  SUPABASE_URL: z.string().default("dummy"),
-  SUPABASE_KEY: z.string().default("dummy"),
-});
+  SUPABASE_URL: z.string().default('dummy'),
+  SUPABASE_KEY: z.string().default('dummy'),
+})
 
 try {
-  const env = await load({ export: true });
-  if (R.isEmpty(env)) throw new Error("no env file found");
+  const env = R.ifElse(
+    R.propEq('test', true),
+    R.always({}),
+    async () => await load(),
+  )(parseArgs(Deno.args))
+
+  if (R.isEmpty(env)) throw new Error('no env file found')
 } catch {
-  console.warn("no env file found, using environment variables");
+  console.warn('no env file found, using environment variables')
 }
 
-export const config = envSchema.parse(Deno.env.toObject());
+export const config = envSchema.parse(Deno.env.toObject())
